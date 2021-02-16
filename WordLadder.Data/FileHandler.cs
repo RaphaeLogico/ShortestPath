b9394954de26fd0a292cd4bcc29d735e534c8b7d
Raphael Oliveira <rfdoliveira@creditoagricola.pt>
2021-02-16 17:36:48 +0000

    Include Data

---
+++ WordLadder.Data/FileHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WordLadder.Data
+{
+    public class FileHandler : IFileHandler
+    {
+        public IEnumerable<string> WordSet { get; set; }
+
+        public IEnumerable<string> LoadDictionaryContent(string filePath)
+        {
+            try
+            {
+                WordSet = File.ReadAllLines(filePath).ToList();
+
+                return WordSet;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error reading the list of words from file path: {filePath}. Error message: {ex.Message}");                
+            }
+        }
+
+        public void SaveOutputFile(string filePath, IEnumerable<string> resultList)
+        {
+            try
+            {
+                File.WriteAllLines(filePath, resultList);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error saving the Result list of words on: {filePath}. Error message: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ WordLadder.Data/IFileHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordLadder.Data
+{
+    interface IFileHandler
+    {
+        /// <summary>
+        /// List of Words
+        /// </summary>
+        public IEnumerable<string> WordSet { get; set; }
+
+        /// <summary>
+        /// Return a List of words generated before the calculation of shortest path between Start Word and End Word provided
+        /// </summary>
+        /// <param name="filePath">Name of the file to Load</param>
+        /// <param name="wordsLength">Size of the words to be loaded</param>        
+        public IEnumerable<string> LoadDictionaryContent(string filePath);        
+
+        /// <summary>
+        /// Save the result of the words after shortest path calculated
+        /// </summary>
+        /// <param name="filePath">Path of the file</param>
+        /// <param name="resultList">List of the words to be saved</param>
+        public void SaveOutputFile(string filePath, IEnumerable<string> resultList);
+
+    }
+}
